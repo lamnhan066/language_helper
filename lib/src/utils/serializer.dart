7408c89f79a8c505e88adfe6e8c1c22ee224674a
VNNiz <contact@niz.vn>
2023-04-19 19:59:37 +0700

    Add serializer for LanguageData

---
+++ lib/src/utils/serializer.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import '../../language_helper.dart';
+import '../models/translate_condition.dart';
+
+String languageDataToJson(LanguageData data) {
+  return jsonEncode(data.map((key, value) {
+    if (value is LanguageCondition) {
+      return MapEntry(key.code, (value as LanguageCondition).toMap());
+    }
+    return MapEntry(key.code, value);
+  }));
+}
+
+LanguageData languageDataFromJson(String data) {
+  final decoded = jsonDecode(data) as Map<String, dynamic>;
+
+  return decoded.map((key, value) {
+    // Reorganize the `value` back to String and LanguageCondition
+    value = value.map((key, value) {
+      //  Try to decode the data back to the LanguageCondition
+      try {
+        final decoded = jsonDecode(value);
+        if (decoded is Map) {
+          return MapEntry(
+              key, LanguageCondition.fromMap(decoded.cast<String, dynamic>()));
+        }
+      } catch (_) {}
+
+      return MapEntry(key, value);
+    });
+    return MapEntry(LanguageCodes.fromCode(key), value.cast<String, dynamic>());
+  });
+}
