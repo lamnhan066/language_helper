28df1380e18a8f38010fbaffbb89e109a001cec4
VNNiz <contact@niz.vn>
2023-04-20 13:16:45 +0700

    Add `toMap` and `fromMap` to LanguageData extension

--- lib/src/extensions/serializer_extension.dart
+++ lib/src/extensions/serializer_extension.dart
@@ -1,10 +1,19 @@
+import 'dart:convert';
+
 import 'package:language_helper/language_helper.dart';
 import 'package:language_helper/src/utils/serializer.dart';
 
 extension LanguageDataSerializer on LanguageData {
   /// Convert this [LanguageData] to JSON
-  String toJson() => languageDataToJson(this);
+  String toJson() => jsonEncode(toMap());
+
+  /// Conver this [LanguageData] to Map
+  Map<String, dynamic> toMap() => languageDataToMap(this);
 
   /// Convert the JSON back to the [LanguageData]
-  static LanguageData fromJson(String json) => languageDataFromJson(json);
+  static LanguageData fromJson(String json) => fromMap(jsonDecode(json));
+
+  /// Convert the Map back to the [LanguageData]
+  static LanguageData fromMap(Map<String, dynamic> map) =>
+      languageDataFromMap(map);
 }
--- lib/src/utils/serializer.dart
+++ lib/src/utils/serializer.dart
@@ -1,9 +1,7 @@
-import 'dart:convert';
-
 import '../../language_helper.dart';
 
-String languageDataToJson(LanguageData data) {
-  return jsonEncode(data.map((key, value) {
+Map<String, dynamic> languageDataToMap(LanguageData data) {
+  return data.map((key, value) {
     value = value.map((key, value) {
       if (value is LanguageConditions) {
         return MapEntry(key, value.toMap());
@@ -13,13 +11,11 @@ String languageDataToJson(LanguageData data) {
     });
 
     return MapEntry(key.code, value);
-  }));
+  });
 }
 
-LanguageData languageDataFromJson(String data) {
-  final decoded = jsonDecode(data) as Map<String, dynamic>;
-
-  return decoded.map((key, value) {
+LanguageData languageDataFromMap(Map<String, dynamic> map) {
+  return map.map((key, value) {
     // Reorganize the `value` back to String and LanguageCondition
     value = (value as Map<String, dynamic>).map((key, value) {
       //  Try to decode the data back to the LanguageCondition
