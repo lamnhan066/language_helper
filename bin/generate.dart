fbbefa258f12d6ad3d6b26b4e6f2ce0bc7e9793e
VNNiz <hello@lamnhan.dev>
2023-08-14 13:29:29 +0700

    Support changing the generating path when using the generator.

--- bin/generate.dart
+++ bin/generate.dart
@@ -1,11 +1,25 @@
+import 'package:args/args.dart';
 import 'package:language_helper_generator/language_helper_generator.dart';
 
 void main(List<String> args) {
   final generator = LanguageHelperGenerator();
 
-  final result = generator.generate();
+  final parser = ArgParser()
+    ..addOption(
+      'path',
+      abbr: 'p',
+      help:
+          'Path to the main folder that you want to to create a base language. Default is `./lib`.',
+      valueHelp: './lib',
+      defaultsTo: './lib',
+    );
+  final argResult = parser.parse(args);
+
+  final path = argResult['path'] as String;
+
+  final result = generator.generate(path);
   if (result == null) return;
 
-  generator.createLanguageDataAbstractFile(result);
-  generator.createLanguageDataFile();
+  generator.createLanguageDataAbstractFile(result, path: path);
+  generator.createLanguageDataFile(path);
 }
