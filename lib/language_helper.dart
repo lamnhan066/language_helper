a079a6e5cc79f64369bbbee0e9dd1cc1a9f7d6f6
VNNiz <hello@lamnhan.dev>
2023-08-18 09:53:35 +0700

    Supports Flutter localizations

--- lib/language_helper.dart
+++ lib/language_helper.dart
@@ -3,6 +3,7 @@ library languages_helper;
 export 'package:language_code/language_code.dart' show LanguageCodes;
 
 export 'src/extensions/language_helper_extension.dart';
+export 'src/extensions/localizations.dart';
 export 'src/extensions/serializer_extension.dart';
 export 'src/language_helper.dart';
 export 'src/models/language_conditions.dart';
---
+++ lib/src/extensions/localizations.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:language_helper/src/language_helper.dart';
+
+extension LanguageLocalizations on LanguageHelper {
+  /// Get the flutter default localizations
+  ///
+  /// Includes:
+  ///     GlobalMaterialLocalizations.delegate
+  ///      GlobalWidgetsLocalizations.delegate
+  ///      GlobalCupertinoLocalizations.delegate
+  List<LocalizationsDelegate> get delegate => [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ];
+}
