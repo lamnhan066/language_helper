de3950b223d482fab69850b0f4d00934000ad625
lamnhan066 <hello@lamnhan.dev>
2024-01-22 12:49:47 +0700

    Export to json from the current LanguageData

--- lib/src/extensions/serializer_extension.dart
+++ lib/src/extensions/serializer_extension.dart
@@ -1,19 +1,35 @@
 import 'dart:convert';
 
 import 'package:language_helper/language_helper.dart';
-import 'package:language_helper/src/utils/serializer.dart';
+import 'package:language_helper/src/utils/serializer.dart' as s;
 
 extension LanguageDataSerializer on LanguageData {
   /// Convert this [LanguageData] to JSON
   String toJson() => jsonEncode(toMap());
 
   /// Conver this [LanguageData] to Map
-  Map<String, dynamic> toMap() => languageDataToMap(this);
+  Map<String, dynamic> toMap() => s.languageDataToMap(this);
+
+  /// Export to json files for `LanguageDataProvider`. Default `path` is set to '.'.
+  ///
+  /// Generated path:
+  /// `path`
+  ///     |- resources
+  ///         |- language_helper
+  ///             |- json
+  ///                 |- codes.json
+  ///                 |- languages
+  ///                     |- en.json
+  ///                     |- vi.json
+  void exportJson([String path = '.']) => s.exportJson(this, path);
 
   /// Convert the JSON back to the [LanguageData]
-  static LanguageData fromJson(String json) => fromMap(jsonDecode(json));
+  static LanguageData fromJson(String json) {
+    return fromMap(jsonDecode(json));
+  }
 
   /// Convert the Map back to the [LanguageData]
-  static LanguageData fromMap(Map<String, dynamic> map) =>
-      languageDataFromMap(map);
+  static LanguageData fromMap(Map<String, dynamic> map) {
+    return s.languageDataFromMap(map);
+  }
 }
--- lib/src/utils/serializer.dart
+++ lib/src/utils/serializer.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:language_helper/src/utils/print_debug.dart';
+
 import '../../language_helper.dart';
 
 Map<String, dynamic> languageDataToMap(LanguageData data) {
@@ -30,4 +35,39 @@ LanguageData languageDataFromMap(Map<String, dynamic> map) {
     });
     return MapEntry(LanguageCodes.fromCode(key), value.cast<String, dynamic>());
   });
+}
+
+void exportJson(LanguageData data, String path) {
+  printDebug('===========================================================');
+  printDebug('Exporting Json...');
+  _exportJsonCodes(data, path);
+  _exportJsonLanguages(data, path);
+  printDebug('Exported Json');
+  printDebug('===========================================================');
+}
+
+void _exportJsonCodes(LanguageData data, String path) {
+  printDebug('Creating codes.json...');
+
+  final desFile = File('$path/resources/language_helper/json/codes.json');
+  desFile.createSync(recursive: true);
+  final codes = data.keys.map((e) => e.code).toList();
+  desFile.writeAsStringSync(jsonEncode(codes));
+
+  printDebug('Created codes.json');
+}
+
+void _exportJsonLanguages(LanguageData data, String path) {
+  printDebug('Creating languages json files...');
+
+  final desPath = '$path/resources/language_helper/json/languages/';
+  final map = languageDataToMap(data);
+  for (final MapEntry(key: String key, value: dynamic value) in map.entries) {
+    final desFile = File('$desPath$key.json');
+    desFile.createSync(recursive: true);
+    final data = jsonEncode({key: value});
+    desFile.writeAsStringSync(data);
+  }
+
+  printDebug('Created languages json files');
 }
