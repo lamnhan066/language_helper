31477f654454d043f1eb0de7c460cfda4ecdd2bc
Lam Thanh Nhan <hello@lamnhan.dev>
2025-11-01 00:27:44 +0700

    feat: disable auto search on scroll in ImproveTranslationButton for better user control

--- example/lib/widgets/improve_translation_button.dart
+++ example/lib/widgets/improve_translation_button.dart
@@ -89,6 +89,7 @@ class ImproveTranslationButton extends StatelessWidget {
                     ),
                   );
                 },
+                autoSearchOnScroll: false,
               ),
             ),
           );
